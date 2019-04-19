c37d4e768da82476ab3ad7454f954702b9cca6e1
Gitanjali Mule <gitanjalidmule@gmail.com>
2019-04-19 02:56:31 +0000

    Automatically backed up by Learn

--- app/models/character.rb
+++ app/models/character.rb
@@ -3,5 +3,9 @@ class Character < ActiveRecord::Base
   belongs_to :show
   belongs_to :network
 
+  def say_that_thing_you_say
+    return "#{self.name} always says: #{self.catchphrase}"
+  end
+
 
 end
